6ea925934afcfb083eb8c2bca3a98cde2d6c80a2
Zeta002 <angeloportelli05@gmail.com>
2024-03-18 10:42:38 +0100

    Initial commit

---
+++ src/main/java/fr/iutamu/iut/book/HelloApplication.java
@@ -0,0 +1,9 @@
+package fr.iutamu.iut.book;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
